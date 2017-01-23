53e853ac367c86e387d42b953cdba8343ca1983b
Anshul Vishwakarma <shootdaj@gmail.com>
2017-01-22 17:45:26 -0800

    Fixed time - it was creating the event at the system date/time instead of the card creation date/time.

--- TrelloGcalSyncer/Syncer.cs
+++ TrelloGcalSyncer/Syncer.cs
@@ -54,12 +54,12 @@ namespace TrelloGcalSyncer
 				Description = card.Description,
 				Start = new EventDateTime()
 				{
-					Date =  card.CreationDate.Date.ToString("yyyy-MM-dd")
-				},
+					Date = card.CreationDate.ToLocalTime().ToString("yyyy-MM-dd")
+                },
 				End = new EventDateTime()
 				{
-					Date = card.CreationDate.Date.ToString("yyyy-MM-dd")
-				}
+					Date = card.CreationDate.ToLocalTime().ToString("yyyy-MM-dd")
+                }
 			};
 			var createRequest = _calendarService.Events.Insert(@event, CalendarID);
 
